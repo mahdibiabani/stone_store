24efb4059cbbc4dc42df72f067d247e77f5307a8
Mahdi Biabani <mahdiibiabani97@gmail.com>
2025-09-17 22:29:53 +0330

    feat: add trusted origin

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -174,7 +174,20 @@ CORS_ALLOW_METHODS = [
 ]
 
 # CSRF settings for frontend integration
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "http://localhost:5174",
+    "http://127.0.0.1:5175",
+    "http://localhost:5175",
+    "http://127.0.0.1:5176",
+    "http://localhost:5176",
+    "http://127.0.0.1:5177",
+    "http://localhost:5177",
+]
 
 # Media files
 MEDIA_URL = '/media/'
